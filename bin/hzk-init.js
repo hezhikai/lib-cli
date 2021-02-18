7551cbc73af0be5b9c9b0d350db52355ae55ae06
hzk <he.zhikai@huafeng.com>
2021-02-18 14:40:47 +0800

    workflow: 增加提交校验

--- bin/hzk-init.js
+++ bin/hzk-init.js
@@ -15,8 +15,10 @@ program.usage('[project-name]');
 program.on('--help', () => {
   console.log('  Examples:');
   console.log();
-  console.log(chalk.gray('    # create a new project with an official template'));
-  console.log('    $ hf init my-project');
+  console.log(
+    chalk.gray('    # create a new project with an official template')
+  );
+  console.log('    $ hzk init my-project');
 });
 
 function help() {
@@ -30,32 +32,35 @@ function downloadAndGenerate() {
   return pipeAsyncFunctions(
     () => {
       if (fs.existsSync(program.args[0])) {
-        return inquirer.prompt([
-          {
-            name: 'cover',
-            type: 'input',
-            message: '组件路径已存在，是否覆盖？',
-            default: 'n'
-          },
-        ]).then(res => {
-          if (res.cover === 'y') {
-            return Promise.resolve();
-          } else {
-            return Promise.reject('请重新输入组件路径');
-          }
-        })
+        return inquirer
+          .prompt([
+            {
+              name: 'cover',
+              type: 'input',
+              message: '组件路径已存在，是否覆盖？',
+              default: 'n'
+            }
+          ])
+          .then((res) => {
+            if (res.cover === 'y') {
+              return Promise.resolve();
+            } else {
+              return Promise.reject('请重新输入组件路径');
+            }
+          });
       }
     },
     () => login(baseConfig.serviceIp),
-    loginToken => axios({
-      method: 'post',
-      baseURL: baseConfig.serviceIp,
-      url: '/nodeService/projectManage/projectTemplate/get',
-      headers: {
-        loginToken
-      }
-    }),
-    res => {
+    (loginToken) =>
+      axios({
+        method: 'post',
+        baseURL: baseConfig.serviceIp,
+        url: '/nodeService/projectManage/projectTemplate/get',
+        headers: {
+          loginToken
+        }
+      }),
+    (res) => {
       if (res.data.success) {
         templates = res.data.data;
         return inquirer.prompt([
@@ -63,62 +68,86 @@ function downloadAndGenerate() {
             name: 'templateName',
             type: 'list',
             message: '选择项目模板',
-            choices: templates.map(item => item.templateName)
+            choices: templates.map((item) => item.templateName)
           },
           {
             name: 'clientId',
             type: 'input',
-            message: '请输入项目ID',
+            message: '请输入项目ID'
           },
           {
             name: 'appName',
             type: 'input',
-            message: '请输入应用名',
+            message: '请输入应用名'
           },
           {
             name: 'proxyPort',
             type: 'input',
-            message: '请输入前端node代理端口号',
-          },
-        ])
+            message: '请输入前端node代理端口号'
+          }
+        ]);
       } else {
         return Promise.reject(res.data.msg);
       }
     },
-    res => {
+    (res) => {
       let local_url = program.args[0];
       let projectName = paramCase(local_url);
-      let web_url = templates.find(temp => temp.templateName === res.templateName).templateUrl;
+      let web_url = templates.find(
+        (temp) => temp.templateName === res.templateName
+      ).templateUrl;
       const spinner = ora('downloading template');
       spinner.start();
-      download(web_url, local_url, err => {
+      download(web_url, local_url, (err) => {
         if (!err) {
           console.info(chalk.blueBright('\n下载成功'));
-          
-          let packageJson = fs.readFileSync(local_url + '/package.json', 'utf-8');
+
+          let packageJson = fs.readFileSync(
+            local_url + '/package.json',
+            'utf-8'
+          );
           packageJson = JSON.parse(packageJson);
           packageJson.name = projectName;
-          fs.writeFileSync(local_url + '/package.json', JSON.stringify(packageJson, null, '\t'), 'utf-8');
-  
+          fs.writeFileSync(
+            local_url + '/package.json',
+            JSON.stringify(packageJson, null, '\t'),
+            'utf-8'
+          );
+
           let envFile = fs.readFileSync(local_url + '/.env', 'utf-8');
-          envFile = envFile.split('\n').map(k => {
-            if (k.includes('clientId')) k = k.replace(/(?<=(clientId *= *))\S+/, res.clientId);
-            if (k.includes('appName')) k = k.replace(/(?<=(appName *= *))\S+/, res.appName);
-            return k;
-          }).join('\n');
+          envFile = envFile
+            .split('\n')
+            .map((k) => {
+              if (k.includes('clientId'))
+                if (k.includes('appName'))
+                  // k = k.replace(/(?<=(clientId *= *))\S+/, res.clientId);
+                  // k = k.replace(/(?<=(appName *= *))\S+/, res.appName);
+                  return k;
+            })
+            .join('\n');
           fs.writeFileSync(local_url + '/.env', envFile, 'utf-8');
-  
-          let vueConfig = fs.readFileSync(local_url + '/vue.config.js', 'utf-8');
-          fs.writeFileSync(local_url + '/vue.config.js', vueConfig.replace(/(?<=service_ip: 'http:\/\/10.1.6.154:)\d+/, res.proxyPort), 'utf-8')
+
+          let vueConfig = fs.readFileSync(
+            local_url + '/vue.config.js',
+            'utf-8'
+          );
+          fs.writeFileSync(
+            local_url + '/vue.config.js',
+            vueConfig.replace(
+              // /(?<=service_ip: 'http:\/\/10.1.6.154:)\d+/,
+              res.proxyPort
+            ),
+            'utf-8'
+          );
         } else {
           console.info(chalk.red('\n下载失败'));
         }
         spinner.stop();
       });
     }
-  )().catch(err => {
+  )().catch((err) => {
     console.info(chalk.red('\n' + err));
-  })
+  });
 }
 
 downloadAndGenerate();
---
+++ bin/utils/index.js
@@ -0,0 +1,74 @@
+const child_process = require('child_process');
+const chalk = require('bin/utils/chalk');
+const rimraf = require('bin/utils/rimraf');
+const fs = require('fs');
+const changeCase = require('bin/utils/change-case');
+
+const pipeAsyncFunctions = (...fns) => (arg) =>
+  fns.reduce((p, f) => p.then(f), Promise.resolve(arg));
+
+exports.pipeAsyncFunctions = pipeAsyncFunctions;
+
+exports.shellExec = (str) => {
+  const exec = child_process.exec;
+  return new Promise((resolve, reject) => {
+    console.log(chalk.cyan(`exec: ${str}`));
+    let data = '';
+    let shell = exec(str, (error) => {
+      if (error) reject();
+    });
+    shell.stdout.on('data', output);
+    shell.stderr.on('data', output);
+    function output(d) {
+      data += d;
+      console.log(d);
+    }
+    shell.on('close', () => {
+      resolve(data);
+    });
+  });
+};
+
+exports.checkCommit = () => {
+  return exports.shellExec('git status').then((data) => {
+    return !data.includes('modified:');
+  });
+};
+
+//删除文件夹
+exports.rmDir = (path) => {
+  if (!fs.existsSync(path)) return Promise.resolve();
+  return new Promise((resolve, reject) => {
+    rimraf(path, (err) => {
+      if (err) {
+        reject();
+      } else {
+        resolve();
+      }
+    });
+  });
+};
+
+//新建
+exports.mkdir = (path) => {
+  if (fs.existsSync(path)) return Promise.resolve();
+  let pathArray = path.split(/\/|\\/g);
+  return pipeAsyncFunctions(
+    ...pathArray.map((item, index, arr) => () => {
+      let tempPath = arr.slice(0, index + 1).join('/');
+      fs.existsSync(tempPath) || fs.mkdirSync(tempPath);
+    })
+  )();
+};
+
+exports.paramCase = (str) => {
+  return changeCase.paramCase(str, {
+    stripRegexp: /[^A-Za-z0-9_]/g
+  });
+};
+
+exports.camelCase = (str) => {
+  return changeCase.camelCase(str, {
+    stripRegexp: /[^A-Za-z0-9_]/g
+  });
+};
